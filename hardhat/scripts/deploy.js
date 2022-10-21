9bfd1b4cfe7914679375dafb5bf28a2dd866ca48
Zach Kelling <z@zeekay.io>
2022-10-21 18:18:02 -0400

    metadata/contract for testnetwork

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,32 +1,29 @@
-const { ethers } = require("hardhat");
-require("dotenv").config({ path: ".env" });
+const { ethers } = require('hardhat')
+require('dotenv').config({ path: '.env' })
 
 async function main() {
   // URL from where we can extract the metadata for a LW3Punks
   const metadataURL =
-    "https://gateway.pinata.cloud/ipfs/QmTkEQ8BvXJnqozxHMigp912rA6b6pJecjgSwZmyPPFN41/";
+    'https://gateway.pinata.cloud/ipfs/QmQgQXFmpMNLZrqvxD7KZS6wqxKaKvjrRyRP1TX73biQYA/'
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
   so luxNftstest here is a factory for instances of our LW3Punks contract.
   */
-  const luxNftstest = await ethers.getContractFactory("LUXTokens");
+  const luxNftstest = await ethers.getContractFactory('LUXTokens')
 
   // deploy the contract
-  const deployedluxNftstest = await luxNftstest.deploy(metadataURL);
+  const deployedluxNftstest = await luxNftstest.deploy(metadataURL)
 
-  await deployedluxNftstest.deployed();
+  await deployedluxNftstest.deployed()
 
   // print the address of the deployed contract
-  console.log(
-    "Lux test network Contract Address:",
-    deployedluxNftstest.address
-  );
+  console.log('Lux test network Contract Address:', deployedluxNftstest.address)
 }
 
 // Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })

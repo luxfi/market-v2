7c6da70ce7d9cff61492842e0220fe9dc0cedcb0
Zach Kelling <z@zeekay.io>
2022-10-24 12:56:06 -0400

    modelv component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,10 +17,18 @@ import Sweep from './Sweep'
 import ReactMarkdown from 'react-markdown'
 import { useMediaQuery } from '@react-hookz/web'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useCollections } from '@reservoir0x/reservoir-kit-ui'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 =======
 >>>>>>> 96757b6 (Update look and feel)
+=======
+import dynamic from 'next/dynamic'
+
+const ModelViewerComp = dynamic(() => import('./ModelViewerComp'), {
+  ssr: false,
+})
+>>>>>>> d51a5d5 (modelv component)
 
 const PROXY_API_BASE = process.env.NEXT_PUBLIC_PROXY_API_BASE
 const envBannerImage = process.env.NEXT_PUBLIC_BANNER_IMAGE
@@ -244,17 +252,25 @@ const Hero: FC<Props> = ({ fallback, collectionId }) => {
     <>
       <HeroBackground banner={header.banner}>
         <div className="z-10 flex w-full flex-col items-center gap-6">
+<<<<<<< HEAD
           <img
 <<<<<<< HEAD
             className={`h-20 w-20 rounded-full ${
               header.image ? 'visible' : 'hidden'
             }`}
 =======
+=======
+          {/* <img
+>>>>>>> 1fd6bd6 (modelv component)
             className="h-20 w-20 rounded-full"
 >>>>>>> d73def8 (initial commit)
             alt={`${header.name} Logo`}
             src={header.image}
-          />
+          /> */}
+
+          <div className="h-20 w-20">
+            <ModelViewerComp></ModelViewerComp>
+          </div>
           <h1 className="reservoir-h4 text-center text-black dark:text-white">
             {header.name}
           </h1>

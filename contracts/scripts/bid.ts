3f9e131b4434c0e2a11ed0aa01bf843dbd144985
sir-memes-alot10000 <sirmemesalot10000@proton.me>
2022-11-11 13:41:54 -0500

    correction

--- contracts/scripts/bid.ts
+++ contracts/scripts/bid.ts
@@ -1,28 +1,30 @@
-// process.env.RESERVOIR_API_KEY
+// // process.env.RESERVOIR_API_KEY
 
-const sdk = require('api')('@reservoirprotocol/v1.0#cpy2fla8spifn');
+// const sdk = require('api')('@reservoirprotocol/v1.0#cpy2fla8spifn');
 
-sdk.auth(process.env.RESERVOIR_API_KEY);
-sdk.server('https://api-goerli.reservoir.tools');
-sdk.postExecuteListV4({
-  params: [
-    {
-      orderKind: 'seaport',
-      orderbook: 'reservoir',
-      automatedRoyalties: true,
-      fees: [
-        '0xaF609ef0f3b682B5992c7A2Ecc0485afD4816d54:10'
-      ],
-      currency: '0x883B256EeD86a9A603C0F98eD7CDE252ce497930',
-      token: '0x46e663972AfE9D500B0A366CdEb8788e39DF1478:220',
-      quantity: 3,
-      weiPrice: '6600000000000000000',
-      listingTime: '1669158515',
-      expirationTime: '1669849715'
-    }
-  ],
-  maker: '0xaF609ef0f3b682B5992c7A2Ecc0485afD4816d54',
-  source: 'lux.market'
-}, {accept: '*/*'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+// sdk.auth(process.env.RESERVOIR_API_KEY);
+// sdk.server('https://api-goerli.reservoir.tools');
+// sdk.postExecuteBidV4({
+//   params: [
+//     {
+//       orderKind: 'seaport',
+//       orderbook: 'reservoir',
+//       automatedRoyalties: true,
+//       fees: [
+//         '0x94B80cCE5F7D6F93119B87d18E3fa8fd3e19350B:100'
+//       ],
+//       excludeFlaggedTokens: false,
+//       collection: '0x46e663972AfE9D500B0A366CdEb8788e39DF1478',
+//       attributeKey: 'Madison North, Rössing Formation, Namibia',
+//       attributeValue: '2000lbs of Uranium',
+//       quantity: 3,
+//       weiPrice: '500000000000000000',
+//       listingTime: '1669158515',
+//       expirationTime: '1669849715'
+//     }
+//   ],
+//   maker: '0xaF609ef0f3b682B5992c7A2Ecc0485afD4816d54',
+//   source: 'lux.market'
+// }, {accept: '*/*'})
+//   .then(({ data }) => console.log(data))
+//   .catch(err => console.error(err));

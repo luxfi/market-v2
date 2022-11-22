ce6a206e52794538257299d7d99df22899653b62
Sangeetha M <sangeetha.m@bzanalytics.ai>
2022-11-22 16:33:30 +0530

    pages added

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -23,7 +23,7 @@ export default function contact() {
             <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Message</label>
             <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Message"></textarea>
           </div>
-            <button type="submit" className="py-3 px-5  w-full text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+            <button type="submit" className="py-3 px-5  w-full text-sm font-medium text-center text-white rounded-lg bg-black focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
         </form>
       </div>
     </section></>

c69446301694dbcd111e6127782181f6669be130
Zach Kelling <z@zeekay.io>
2023-01-12 00:43:56 -0600

    Refactor into yarn workspace
    Update workspace
    
    Merge efforts

--- contracts/scripts/0001_vera_metadata.ts
+++ contracts/scripts/0001_vera_metadata.ts
@@ -5,11 +5,6 @@ import { NFTStorage, File } from 'nft.storage';
 import mime from 'mime';
 import path from 'path';
 
-<<<<<<< HEAD:contracts/scripts/0001_vera_metadata.ts
-import { generate, Template} from "@pdfme/generator/";
-const template = require("./pdfs/template.json");
-//     LUX-MM-U3O8-0000-<tokenID>
-=======
 import { generate } from "@pdfme/generator/";
 
 
@@ -17,14 +12,13 @@ const template = require("./template.json");
 
 
 
->>>>>>> develop:contracts/scripts/verametadata.ts
 
 require("dotenv").config({ path: ".env" });
 
 const NFT_STORAGE_KEY = process.env.NFT_STORAGE_KEY;
 
-function meta_data( tokenId: number, 
-                    desc: string, 
+function meta_data( tokenId: number,
+                    desc: string,
                     amt_lbs: number) : string {
 
     let data = {
@@ -90,7 +84,7 @@ function meta_data( tokenId: number,
             {
             "address": "0x6cB50C54953ac3d8D622BF6B2A9F68B48dF4773A",
             "share": 100
-            }       
+            }
         ]
         },
         "animation_url": "https://bafybeibajrcv6iuleltwr6jnwn3ggzzyc2sonbns3gcjjvy73q2fa6lewe.ipfs.nftstorage.link"
@@ -115,18 +109,12 @@ async function mint_nfts(start_id: number, end_id: number, poundage: number, des
                 flag: "a+",
                 mode: 0o666
             }
-        );    
+        );
     }
 }
 
-<<<<<<< HEAD:contracts/scripts/0001_vera_metadata.ts
-async function render_pdf(tokenId: number, poundage: number) {  
-    const SPOT_PRICE:number  = 50.81;
-=======
 async function render_pdf(tokenId: number, poundage: number) {
-          
-}
->>>>>>> develop:contracts/scripts/verametadata.ts
+    const SPOT_PRICE:number  = 50.81;
 
         const inputs = [
         {
@@ -145,10 +133,9 @@ async function render_pdf(tokenId: number, poundage: number) {
         },
     ];
 
-<<<<<<< HEAD:contracts/scripts/0001_vera_metadata.ts
-    const pdf = await generate({ template, inputs });   
+    const pdf = await generate({ template, inputs });
     let pdfpath = `${process.cwd()}/scripts/render/${tokenId}.pdf`;
-  
+
     await fs.writeFile(pdfpath,
         pdf,
         {
@@ -158,25 +145,8 @@ async function render_pdf(tokenId: number, poundage: number) {
         }
     );
 }
-=======
-    const inputs = [
-        {}
-      ];
-    const pdf = await generate({ template, inputs });
 
-    let pdfpath = `${process.cwd()}/scripts/uranium/0.pdf`;
-
-        await fs.writeFile(pdfpath,
-            pdf,
-            {
-                encoding: "utf8",
-                flag: "a+",
-                mode: 0o666
-            }
-        );
->>>>>>> develop:contracts/scripts/verametadata.ts
-
-async function main() {  
+async function main() {
 
     // // ONE POUND x 0 - 999
     await mint_nfts(0, 1000, 1, "Backed by one pound of Uranium (U₃O₈) from the Madison North mine.")
@@ -188,7 +158,7 @@ async function main() {
     await mint_nfts(1100, 1110, 100, "Backed by one hundred pounds of Uranium (U₃O₈) from the Madison North mine.")
 
     // TWO THOUSAND Pounds x 1???
-    await mint_nfts(1110, 1111, 2000, "Backed by one ton of Uranium (U₃O₈) from the Madison North mine.") 
+    await mint_nfts(1110, 1111, 2000, "Backed by one ton of Uranium (U₃O₈) from the Madison North mine.")
 
 }
 
@@ -198,8 +168,8 @@ main()
     console.error(error);
     process.exit(1);
 });
-	
-// 2,000	    1	        2,000	$70,000	$70,000			
+
+// 2,000	    1	        2,000	$70,000	$70,000
 // 100	        10	        1,000	$35,000	$3,500
 // 10	        100	        1,000	$35,000	$350
 // 1        	1,000   	1,000	$35,000	$35

25aa17416ac802a911b069a823faf2340b979e8f
Zach Kelling <z@zeekay.io>
2023-01-17 04:09:41 -0600

    Update collection link

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -63,7 +63,7 @@ const Home: NextPage<Props> = ({ fallback }) => {
   const isSmallDevice = useMediaQuery('only screen and (max-width : 600px)')
 
   const router = useRouter()
-  const tokenHref = `/collections/0x31e0F919C67ceDd2Bc3E294340Dc900735810311`
+  const tokenHref = `/collections/0x31e0f919c67cedd2bc3e294340dc900735810311/`
 
   const title = META_TITLE && metadata.title(META_TITLE)
   const description = META_DESCRIPTION && metadata.description(META_DESCRIPTION)
